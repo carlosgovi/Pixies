3f96b2dc46ec503702c5576cbbe24488b3256251
carlosgovi <carlosgovi.dev@gmail.com>
2023-09-21 22:03:37 -0300

    add layout force dynamic for deploy problem cookies server side

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,7 +1,4 @@
-import {
-  createRouteHandlerClient,
-  createServerComponentClient,
-} from "@supabase/auth-helpers-nextjs";
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
